f407c7d0e0481969ed00df4b3b9337f66c359e00
R. Kent James <kent@caspia.com>
2024-07-27 11:04:48 -0700

    More rosdoc2 fixes, use only-iron rosindex

--- rosdoc2/run.sh
+++ rosdoc2/run.sh
@@ -3,4 +3,4 @@ echo "rosdoc2 run.sh"
 python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install rosdoc2
-rosdoc2 scan -p 'plugins_data/checkout'
+rosdoc2 scan -p '_plugins_data/checkout'
