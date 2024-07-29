0682c5a126e8d16717ccbdea2ea6693bf48c8073
R. Kent James <kent@caspia.com>
2024-07-28 20:55:43 -0700

    Fixes in rosdoc2

--- rosdoc2/run.sh
+++ rosdoc2/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-echo "rosdoc2 run.sh"
 python3 -m venv venv
 source venv/bin/activate
-python3 -m pip install rosdoc2
+python3 -m pip install ./rosdoc2
 rosdoc2 scan -p '_plugins_data/checkout'
