64e36831cc069208b199e56e6f95629ed6f69cda
R. Kent James <kent@caspia.com>
2024-07-29 17:04:51 -0700

    Separate rosdoc2 and rosindex directories

--- rosdoc2/run.sh
+++ rosdoc2/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 python3 -m venv venv
 source venv/bin/activate
+python3 -m pip install osrf-pycommon
 python3 -m pip install ./rosdoc2
-rosdoc2 scan -p '_plugins_data/checkout'
+rosdoc2 scan -p 'rosindex/_plugins_data/checkout'
