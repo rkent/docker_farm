438fb70191a13b8ecc391539b81766f70ea325f1
R. Kent James <kent@caspia.com>
2024-07-30 08:58:43 -0700

    Tweaks to rosdoc2: enable rsync, ignore _release_repos

--- rosdoc2/run.sh
+++ rosdoc2/run.sh
@@ -3,4 +3,5 @@ python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install osrf-pycommon
 python3 -m pip install ./rosdoc2
+touch rosindex/_plugins_data/checkout/_release_repos/CATKIN_IGNORE || true
 rosdoc2 scan -p 'rosindex/_plugins_data/checkout'
