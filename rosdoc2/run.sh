b34f5cbe3ae266fe3edfc39af215594c1326d3ec
R. Kent James <kent@caspia.com>
2024-08-15 10:37:15 -0700

    Add big packages with external documentation to rosdoc2

--- rosdoc2/run.sh
+++ rosdoc2/run.sh
@@ -4,4 +4,5 @@ source venv/bin/activate
 python3 -m pip install osrf-pycommon
 python3 -m pip install ./rosdoc2
 touch rosindex/_plugins_data/checkout/_release_repos/CATKIN_IGNORE || true
+export ROS_DISTRO='iron'
 rosdoc2 scan -p 'rosindex/_plugins_data/checkout' -t 3600 -y 'rosdoc2/docs_ex.yaml'
