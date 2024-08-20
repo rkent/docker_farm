7085e03760def603a41b02bd7c70937d2ce302db
R. Kent James <kent@caspia.com>
2024-08-19 23:07:55 -0700

    Switch to distroclone for repo checkout in rosdoc2

--- rosdoc2/run.sh
+++ rosdoc2/run.sh
@@ -3,6 +3,7 @@ python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install osrf-pycommon
 python3 -m pip install ./rosdoc2
-touch rosindex/_plugins_data/checkout/_release_repos/CATKIN_IGNORE || true
+python3 -m pip install ./distroclone
 export ROS_DISTRO='rolling'
-rosdoc2 scan -p 'rosindex/_plugins_data/checkout' -t 3600 -y 'rosdoc2/docs_ex.yaml'
+distroclone -d $ROS_DISTRO -p checkout
+rosdoc2 scan -p 'checkout' -t 3600 -y 'rosdoc2/docs_ex.yaml'
