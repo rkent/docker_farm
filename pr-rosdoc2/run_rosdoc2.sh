a7c849586bacabafd9570ff1fe11a6c0361b73ef
R. Kent James <kent@caspia.com>
2024-10-07 12:24:20 -0700

    Use /checkout in pr-rosdoc2

--- pr-rosdoc2/run_rosdoc2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-python3 -m venv venv
-source venv/bin/activate
-python3 -m pip install osrf-pycommon
-python3 -m pip install ./rosdoc2
-export ROS_DISTRO='rolling'
-rosdoc2 scan -p 'checkout' -t 3600
