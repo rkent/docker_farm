7f79de0b9e3e430431dc0cc7fad7946ec588ec45
R. Kent James <kent@caspia.com>
2024-10-07 11:58:47 -0700

    Add pr-rosdoc2

--- pr-rosdoc2/run_rosdoc2.sh
+++ pr-rosdoc2/run_rosdoc2.sh
@@ -4,4 +4,4 @@ source venv/bin/activate
 python3 -m pip install osrf-pycommon
 python3 -m pip install ./rosdoc2
 export ROS_DISTRO='rolling'
-rosdoc2 $1
+rosdoc2 scan -p 'checkout' -t 3600 -y 'rosdoc2/docs_ex.yaml'
