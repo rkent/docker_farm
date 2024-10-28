146d92ca0de00e704105e9023c58fa850b7cface
R. Kent James <kent@caspia.com>
2024-10-27 22:36:50 -0700

    rosdoc2_rolling use common for Dockerfile

--- rosdoc2_rolling/run_rosdoc2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export ROS_DISTRO=${ROS_DISTRO:-'rolling'}
-python3 -m venv venv
-source venv/bin/activate
-python3 -m pip install osrf-pycommon
-python3 -m pip install ./rosdoc2
-rosdoc2 $1
