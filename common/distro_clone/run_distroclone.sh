071b70960dd84217a85797d90d95a72596baf7ed
R Kent James <kent@caspia.com>
2025-05-03 15:48:48 -0700

    Spec github path in distroclone

--- common/distro_clone/run_distroclone.sh
+++ common/distro_clone/run_distroclone.sh
@@ -3,14 +3,15 @@
 ROS_DISTRO=${1:-'rolling'}
 DESTINATION=${2:-'checkout'}
 REPO_MERGE_FILE=${3:-''}
+GITHUB_PATH='repo_multi/distroclone/github'
 python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install osrf-pycommon
 python3 -m pip install ./distroclone
 if [[ $REPO_MERGE_FILE ]]; then
     echo "Checking out $ROS_DISTRO to destination $DESTINATION merging $REPO_MERGE_FILE"
-    distroclone -d $ROS_DISTRO -p $DESTINATION -c $REPO_MERGE_FILE
+    distroclone -d $ROS_DISTRO -p $DESTINATION -c $REPO_MERGE_FILE -g $GITHUB_PATH
 else
     echo "Checking out $ROS_DISTRO to destination $DESTINATION"
-    distroclone -d $ROS_DISTRO -p $DESTINATION
+    distroclone -d $ROS_DISTRO -p $DESTINATION -g $GITHUB_PATH
 fi
